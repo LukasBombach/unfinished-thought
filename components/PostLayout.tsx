b25e60c48461a68ea35ad16f0e40d9bfe0c3d6b0
Lukas Bombach <lukas.bombach@googlemail.com>
2021-12-21 14:17:28 +0100

    post styling wip

--- components/PostLayout.tsx
+++ components/PostLayout.tsx
@@ -20,19 +20,31 @@ const Wrapper = styled("div", {
   maxWidth: 720,
 });
 
+const Title = styled("h1", {
+  fontSize: "2.2rem",
+  lineHeight: "1.3",
+});
+
 const Paragraph = styled("p", {
-  lineHeight: "$16",
+  lineHeight: "1.9",
+});
+
+const Headline = styled("h2", {
+  fontSize: "1.8rem",
+  lineHeight: "1.3",
 });
 
 const components = {
   p: Paragraph,
+  h2: Headline,
 };
 
 export const PostLayout: FC<{ meta: PostMeta }> = ({ meta, children }) => {
   return (
     <Wrapper>
       <PageTitle pageName={meta.title} />
-      <h1>{meta.title}</h1>
+      <Title>{meta.title}</Title>
+
       <MDXProvider components={components}>{children}</MDXProvider>
     </Wrapper>
   );
