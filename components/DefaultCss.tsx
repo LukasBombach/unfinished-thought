1512d1b419b92caf954562490c67356ba8c1d4d1
Lukas Bombach <lukas.bombach@googlemail.com>
2021-12-22 12:23:41 +0100

    bg color

--- components/DefaultCss.tsx
+++ components/DefaultCss.tsx
@@ -2,6 +2,8 @@ import { globalCss } from "stitches.config";
 
 import type { VFC } from "react";
 
+// [1] todo bg is not height 100% in all cases
+// [1] todo not a token yet and I don't like the color
 const applyDefaultCss = globalCss({
   "html, body, h1, h2, p, blockquote, ul, li, div, pre": {
     margin: 0,
@@ -9,6 +11,9 @@ const applyDefaultCss = globalCss({
     lineHeight: "1",
     boxSizing: "border-box",
     fontFamily: "$sans",
+  },
+  body: {
+    background: "#fbf7f010", // [1]
   },
   a: { color: "inherit", textDecoration: "none" },
   ul: { listStyle: "none" },
