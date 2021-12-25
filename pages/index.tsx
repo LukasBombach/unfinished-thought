de593327a719f66e9d007f531bb970f1986484c3
Lukas Bombach <lukas.bombach@googlemail.com>
2021-12-25 20:10:32 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import { readdir } from "fs/promises";
+import { basename } from "path";
 import { Layout } from "components/Layout";
 import { Header } from "components/Header";
-
 import { JustOnePost } from "components/JustOnePost";
 
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+
+interface PostMeta {
+  title: string;
+  description: string;
+  date: Date;
+  relativeUrl: string;
+}
 
 const Home: NextPage = () => {
   return (
@@ -13,5 +21,36 @@ const Home: NextPage = () => {
     </Layout>
   );
 };
+
+export const getStaticProps: GetStaticProps = async context => {
+  const files = await readdir(process.cwd());
+  const postFiles = files.filter(n => n.endsWith(".mdx"));
+  const posts = await Promise.all(postFiles.map(file => import(file)));
+
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+};
+
+async function getPostMeta(path: string): Promise<PostMeta> {
+  const post = await import(path);
+  const { title, description, date } = post?.meta ?? {};
+  const relativeUrl = basename(path, ".mdx");
+  const postMeta = { title, description, date, relativeUrl };
+  assertPostMeta(postMeta);
+  return postMeta;
+}
+
+function assertPostMeta(val: unknown): asserts val is PostMeta {
+  if (val === null || typeof val !== "object") {
+    throw new Error(`${val} is not if type PostMeta`);
+  }
+}
+
+function assertIsString(value: any): asserts value is number {
+  if (typeof value !== "number") {
+    throw new TypeError();
+  }
+}
 
 export default Home;
